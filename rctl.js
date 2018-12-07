2f2dd0a06026f3070ad06f92d2d06dcced916a59
Fonoster Team <fonosterteam@fonoster.com>
2018-12-06 21:44:29 -0500

    Fixed incorrect command parsing

--- rctl.js
+++ rctl.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 const shell = require("shelljs")
-const args = process.argv.slice(2, process.argv.length).join().replace(',', ' ')
+const args = process.argv.slice(2, process.argv.length).join().replace(/,/g, ' ')
+console.log('args: '  + args)
 const cmd = "java -cp " + __dirname + "/libs/routr-ctl-all.jar -Dcom.sun.net.ssl.checkRevocation=true com.fonoster.routr.ctl.Main " + args
 shell.exec(cmd)
