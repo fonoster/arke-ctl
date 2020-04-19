bf32001674507e985a80124dae8fd74eba0eee98
Pedro Sanders <sanderspedro@gmail.com>
2020-04-19 16:48:12 -0400

    Added version fom package

--- rctl.js
+++ rctl.js
@@ -4,4 +4,5 @@ const scapedArgs = process.argv.map(arg => arg = encodeURI(arg).replace(/'/g, "\
 const args = scapedArgs.slice(2, process.argv.length).join().replace(/,/g, ' ')
 const cmd = `java -Dfile.encoding=UTF-8 -cp ${__dirname}/libs/routr-ctl-all.jar -Dcom.sun.net.ssl.checkRevocation=true io.routr.ctl.Main ${args}`
 process.env.ROUTR_WEBAPP=`${__dirname}/libs/webapp`
+process.env.ROUTR_CTL_VERSION=require('./package.json').version
 shell.exec(cmd)
