e9b2830c802ef8fe4be09f47709ea134dbbb222d
Pedro Sanders <sanderspedro@gmail.com>
2020-05-03 21:22:51 -0400

    Updated snapcraft distro artifacts

--- rctl.js
+++ rctl.js
@@ -4,11 +4,12 @@ const cmdExists = require('command-exists').sync;
 const shell = require('shelljs')
 const scapedArgs = process.argv.map(arg => arg = encodeURI(arg).replace(/'/g, "\\'"))
 const args = scapedArgs.slice(2, process.argv.length).join().replace(/,/g, ' ')
-const cmd = `java -Dlog4j.configurationFile=${__dirname}/log4j2.yml -Dfile.encoding=UTF-8 -cp ${__dirname}/libs/routr-ctl-all.jar -Dcom.sun.net.ssl.checkRevocation=true io.routr.ctl.Main ${args}`
+const home = process.env.JAVA_HOME ? process.env.JAVA_HOME + '/bin/' : ''
+const cmd = `${home}java -Dlog4j.configurationFile=${__dirname}/log4j2.yml -Dfile.encoding=UTF-8 -cp ${__dirname}/libs/routr-ctl-all.jar -Dcom.sun.net.ssl.checkRevocation=true io.routr.ctl.Main ${args}`
 
 // NOTICE: This overlaps a functionality of the Main method of getting
 // apiUrl and token. This was necessary because there is no apparent way to
-// pass parameters the WSHandler, so we are using an environment variables.
+// pass parameters to the WSHandler, so we are using an environment variables.
 try {
   const config = require(CONFIG_PATH)
   process.env.ROUTR_API_URL=config.apiUrl
@@ -18,7 +19,7 @@ process.env.ROUTR_WEBAPP=`${__dirname}/libs/webapp`
 process.env.ROUTR_CTL_VERSION=require('./package.json').version
 
 // Ensure java command is available
-if (cmdExists('java')) {
+if (process.env.JAVA_HOME || cmdExists('java')) {
     shell.exec(cmd)
 } else {
     console.log('Could not find a runtime environment. Please setup the JAVA_HOME environment variable')
